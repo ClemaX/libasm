9fd807494fe65192ef30efe446a73a34f2b54f9a
Clément Hamada <clementhamada@protonmail.com>
2020-02-07 10:57:25 +0100

    Initial ft_strlen

---
+++ ft_strlen.s
@@ -0,0 +1,13 @@
+section	.text
+global _ft_strlen
+
+_ft_strlen:
+	sub		rcx, rcx	; Clear RCX
+	not		rcx			; Set RCX to unsigned-max
+	sub		al, al		; Clear AL
+	cld					; Clear DF / go forward
+	repne	scasb		; Scan byte-string
+	not		rcx			; Invert bits on RCX
+	dec		rcx			; Decrement RCX
+	mov		rax, rcx	; Set RAX to RCX
+	ret					; Return
