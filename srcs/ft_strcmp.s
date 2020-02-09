df445ecfd924b9e27cc5306e3de420c068709cbe
Clément Hamada <clementhamada@protonmail.com>
2020-02-09 17:39:51 +0100

    Cleanup strcmp

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -2,26 +2,26 @@ section	.text
 global	_ft_strcmp
 
 _ft_strcmp:					; RDI, RSI
-	match:					; While the strings match
-	mov		al, [rdi]	;
-	mov		bl, [rsi]	;
+	.match:					; While the strings match
+	mov		al, [rdi]		; Read the actual chars
+	mov		bl, [rsi]		;
 	cmp		al, bl			; Compare both chars
-	ja		above
-	jb		below
+	ja		.above			;
+	jb		.below			;
 	test	al, al			; Check for terminator
-	jz		zero
-	inc		rdi				; Increment counter
-	inc		rsi				; Increment counter
-	jmp		match			;
+	jz		.zero			;
+	inc		rdi				; Increment pointers
+	inc		rsi				;
+	jmp		.match			;
 
-zero:
+.zero:
 	mov		eax, 0			; Return 0
 	ret
 
-above:
+.above:
 	mov		eax, 1			; Return 1
 	ret
 
-below:
+.below:
 	mov		eax, -1			; Return -1
 	ret
