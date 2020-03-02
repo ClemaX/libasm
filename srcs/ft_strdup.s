67d20801d0a29c7a9e9b902b75e20fb305355147
Clément Hamada <clementhamada@protonmail.com>
2020-03-02 15:54:02 +0100

    Fix ft_strdup

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -5,12 +5,14 @@ extern	_ft_strlen
 extern	_ft_strncpy
 
 _ft_strdup:				; RDI
-	mov		rsi, rdi	; Set RSI to src
+	push	rdi			; Save src
 	call	_ft_strlen	; ft_strlen(RDI)
-	inc		rax			; Increment length
-	mov		rcx, rax	; Set RDI to size
-	mov		rdx, rax	; Set RDX to size
-	call	_malloc		; malloc(RCX)
+	inc		rax			; Increment len
+	mov		rdi, rax	; Set RDI to size
+	push	rax			; Save size
+	call	_malloc		; malloc(RDI)
 	mov		rdi, rax	; Set RDI to dst
+	pop		rdx			; Set RDX to size
+	pop		rsi			; Set RSI to src
 	call	_ft_strncpy	; ft_strncpy(RDI, RSI, RDX)
 	ret
