8c37f057e414171767f9d36bfa812a329c3e306e
Clément Hamada <clementhamada@protonmail.com>
2020-03-10 19:29:38 +0100

    DONE

--- srcs/ft_atoi_base.s
+++ srcs/ft_atoi_base.s
@@ -1,5 +1,5 @@
-section .data
-buff:		times 32 dq -1		; 256 bytes writeable zero-buff
+section	.data
+buff:	times 256 db -1		; 256 bytes writeable zero-buff
 
 section	.text
 global	_ft_atoi_base
@@ -7,7 +7,7 @@ global	_ft_atoi_base
 _ft_atoi_base:					; RDI, RSI
 	sub		rcx,	rcx			; Clear RCX
 	sub		rax,	rax			; Clear RAX
-	mov		rdx,	buff		; 
+	lea		rdx,	[rel buff]	; 
 .validate:
 	mov		al,		[rsi + rcx]	; Read a char at RCX
 	cmp		al,		'+'			; Check for invalid chars
@@ -17,16 +17,17 @@ _ft_atoi_base:					; RDI, RSI
 	mov		bl,		[rdx + rax]	; Load byte according to character
 	cmp		bl,		-1			; Check if character has occurred
 	jne		.error				; Cannot have duplicates
+	test	al,		al			; Check for terminator
+	jz		.validated			; Loop until terminator
 	mov		[rdx + rax], cl		; Put position into buffer
 	inc		rcx					; Increment RCX
-	test	al,		al			; Check for terminator
-	jnz		.validate			; Loop until terminator
-	sub		rcx,	1			; Get length from size
+	jmp		.validate
+.validated:
 	cmp		rcx,	2			; Check minimum length
 	jb		.error				; Return if below
 	sub		al,		al			; Clear AL
 	sub		rbx,	rbx			; Clear RBX
-	sub		rsi,	rsi			; Set RSI to 0
+	sub		si,	si				; Set RSI to 0
 .prefix:
 	mov		bl,		[rdi]		; Read a char from RDI
 	cmp		bl,		'+'			; Skip '+' sign
@@ -39,24 +40,28 @@ _ft_atoi_base:					; RDI, RSI
 	je		.skip				;
 .sign:
 	cmp		bl,		'-'			; Continue if there is no sign 
-	jne		.convert
-	inc		rsi					; Increment '-' counter TODO: inc si should be enough
+	jne		.root
+	inc		si					; Increment '-' counter TODO: inc si should be enough
 .skip:
 	inc		rdi					; Increment RDI
 	jmp		.prefix				; TODO: push rsi
+.root:
+	push	si					; Save RSI ('-'-parity)
+	mov		rsi,	rdx			; Set RSI to RDX
 .convert:
-	mov		bl,		[rdx + rbx]	; Read the characters magnitude
+	mov		bl,		[rsi + rbx]	; Read the characters magnitude
 	cmp		bl,		-1			; Check for invalid character
-	je		.exit
+	je		.negate
 	mul		ecx					; Multiply EAX by ECX
 	add		eax,	ebx			; Add the characters magnitude
 	inc		rdi					; Increment RDI
 	mov		bl,		[rdi]		; Read a char from RDI
 	jmp		.convert			; Loop
 .negate:
-	test	si,	1				; Check parity
+	pop		si					; Restore RSI ('-'-parity)
+	test	si,		1			; Check parity
 	jz		.exit
-	neg		eax
+	neg		eax					; Negate EAX
 .exit:
 	ret
 .error:
