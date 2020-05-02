edda48161cb620d1de95aa5293056160fb1733da
Clément Hamada <clementhamada@protonmail.com>
2020-05-02 16:45:47 +0200

    Fix NULL check and push/pop RSI

--- srcs/ft_list_remove_if.s
+++ srcs/ft_list_remove_if.s
@@ -9,10 +9,13 @@ _ft_list_remove_if:
 	mov		r13,	[r12]		; Set R13 to list pointer
 	mov		r15,	rdx			; Set R15 to (*cmp)()
 	mov		rbx,	rcx			; Set RBX to (*free_fct)(void*)
+	push	rsi					; Store *data_ref
 .start:
 	test	r13,	r13			; Check for NULL
 	jz		.end				; End on NULL
 	mov		rdi,	[r13]		; Set RDI to data pointer
+	pop		rsi					; Restore *data_ref
+	push	rsi					; Store *data_ref
 	call	r15					; cmp(RDI, RSI)
 	test	eax,	eax			; Check for match
 	jnz		.loop				; Continue loop if no match
@@ -24,20 +27,24 @@ _ft_list_remove_if:
 	jmp		.start				; Loop on next pointer
 .loop:
 	mov		r14,	[r13 + 8]	; Set R14 to next pointer
+	test	r14,	r14			; Check for NULL
+	jz		.end				; End on NULL
 	mov		rdi,	[r14]		; Set RDI to data pointer
+	pop		rsi					; Restore *data_ref
+	push	rsi					; Store *data_ref
 	call	r15					; cmp(RDI, RSI)
 	test	eax,	eax			; Check for match
 	jz		.delete				; Delete if matching
 	mov		r13,	r14			; Set R13 to list pointer
-	test	r14,	r14			; Check for NULL
-	jnz		.loop				; End on NULL
+	jmp		.loop				; End on NULL
 .end:
+	pop		rsi					; Restore RSI
 	ret
 .delete:
 	call	rbx					; free_fct(RDI)
 	mov		rdi,	r14			; Set RDI to list pointer
 	mov		r14,	[r14 + 8]	; Set R14 to next pointer
-	call	_free
+	call	_free				; free(RDI)
 	mov		[r13 + 8], r14		; Set previous next pointer to R14
 	jmp		.loop				; Continue loop
 
