ce3c9453c2cadef45ab85650aab55814edcc4dfc
Clément Hamada <clementhamada@protonmail.com>
2020-05-02 15:36:08 +0200

    Use non-volatile registers for persistence

--- srcs/ft_list_remove_if.s
+++ srcs/ft_list_remove_if.s
@@ -1,20 +1,22 @@
 section	.text
 ;void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
-global	_ft_list_remove_if		; RDI, RSI, RDX, RCX
+global	_ft_list_remove_if		; RDI, RSI, RDX, RCX - RBX, R12, R13, R14, R15
 
 extern	_free
 
 _ft_list_remove_if:
 	mov		r12,	rdi			; Set R12 to RDI
 	mov		r13,	[r12]		; Set R13 to list pointer
+	mov		r15,	rdx			; Set R15 to (*cmp)()
+	mov		rbx,	rcx			; Set RBX to (*free_fct)(void*)
 .start:
 	test	r13,	r13			; Check for NULL
 	jz		.end				; End on NULL
 	mov		rdi,	[r13]		; Set RDI to data pointer
-	call	rdx					; cmp(RDI, RSI)
+	call	r15					; cmp(RDI, RSI)
 	test	eax,	eax			; Check for match
 	jnz		.loop				; Continue loop if no match
-	call	rcx					; free_fct(RDI)
+	call	rbx					; free_fct(RDI)
 	mov		rdi,	r13			; Set RDI to list pointer
 	mov		r13,	[r13 + 8]	; Set R13 to next pointer
 	call	_free				; free(RDI)
@@ -23,7 +25,7 @@ _ft_list_remove_if:
 .loop:
 	mov		r14,	[r13 + 8]	; Set R14 to next pointer
 	mov		rdi,	[r14]		; Set RDI to data pointer
-	call	rdx					; cmp(RDI, RSI)
+	call	r15					; cmp(RDI, RSI)
 	test	eax,	eax			; Check for match
 	jz		.delete				; Delete if matching
 	mov		r13,	r14			; Set R13 to list pointer
@@ -32,9 +34,10 @@ _ft_list_remove_if:
 .end:
 	ret
 .delete:
-	call	rcx					; free_fct(RDI)
+	call	rbx					; free_fct(RDI)
 	mov		rdi,	r14			; Set RDI to list pointer
 	mov		r14,	[r14 + 8]	; Set R14 to next pointer
 	call	_free
 	mov		[r13 + 8], r14		; Set previous next pointer to R14
 	jmp		.loop				; Continue loop
+
