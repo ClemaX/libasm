fe6837d38eacdd06e21c70ce8dcf7fb5a6843730
Clément Hamada <clementhamada@protonmail.com>
2020-05-18 16:05:09 +0200

    Use Linux syscalls

--- srcs/ft_read.s
+++ srcs/ft_read.s
@@ -4,7 +4,8 @@ global	ft_read
 ft_read:					; RDI, RSI, RDX
 	test	rdx, rdx		; Check for negative length
 	js		error			;
-	mov		rax, 0x2000003	; Read Call #
+	mov		rax, 0			; Read Call # (Linux)
+;	mov		rax, 0x2000003	; Read Call # (BSD)
 	syscall					; Call the system
 	jc		error			; Check for error
 	ret
--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -4,7 +4,8 @@ global	ft_write
 ft_write:					; RDI, RSI, RDX
 	test	rdx, rdx
 	js		error			;
-	mov		rax, 0x2000004	; Write Call #
+	mov		rax, 1			; Write Call # (Linux)
+;	mov		rax, 0x2000004	; Write Call # (BSD)
 	syscall					; Call the system
 	jc		error			; Check for error
 	ret
