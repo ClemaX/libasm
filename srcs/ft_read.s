05e6c08efa60781b0082cff0a047610924ce1976
Clément Hamada <clementhamada@protonmail.com>
2020-05-18 16:05:23 +0200

    Handle Linux errno

--- srcs/ft_read.s
+++ srcs/ft_read.s
@@ -3,13 +3,14 @@ global	ft_read
 
 ft_read:					; RDI, RSI, RDX
 	test	rdx, rdx		; Check for negative length
-	js		error			;
+	js		.error			;
 	mov		rax, 0			; Read Call # (Linux)
 ;	mov		rax, 0x2000003	; Read Call # (BSD)
 	syscall					; Call the system
-	jc		error			; Check for error
+	test	rax, rax		; Check for error (Linux)
+	js		.error
+;	jc		.error			; Check for error (BSD)
 	ret
-
-error:
+.error:
 	mov		rax, -1	; Return -1
 	ret
--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -2,14 +2,15 @@ section	.text
 global	ft_write
 
 ft_write:					; RDI, RSI, RDX
-	test	rdx, rdx
-	js		error			;
+	test	rdx, rdx		; Check for negative length
+	js		.error			;
 	mov		rax, 1			; Write Call # (Linux)
 ;	mov		rax, 0x2000004	; Write Call # (BSD)
 	syscall					; Call the system
-	jc		error			; Check for error
+	test	rax, rax		; Check for error (Linux)
+	js		.error
+;	jc		.error			; Check for error (BSD)
 	ret
-
-error:
+.error:
 	mov		rax, -1	; Return -1
 	ret
