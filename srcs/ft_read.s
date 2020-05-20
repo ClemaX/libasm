78e6b929fd8b330eae2ffe0b464fde3d52120c84
Clément Hamada <clementhamada@protonmail.com>
2020-05-20 19:18:08 +0200

    Syscalls compatible for macOS and Linux

--- srcs/ft_read.s
+++ srcs/ft_read.s
@@ -1,3 +1,16 @@
+%ifidn __OUTPUT_FORMAT__, elf64
+	%assign READ_CALL 0
+	%macro JERR 1
+		test	rax, rax	; Check for error (Linux)
+		js		%1
+	%endmacro
+%elifidn __OUTPUT_FORMAT__, macho64
+	%assign READ_CALL 0x2000003
+	%macro JERR 1
+		jc		%1			; Check for error (BSD)
+	%endmacro
+%endif
+
 section	.text
 global	ft_read
 extern	__errno_location
@@ -5,12 +18,9 @@ extern	__errno_location
 ft_read:					; RDI, RSI, RDX
 	test	rdx, rdx		; Check for negative length
 	js		.error			;
-	mov		rax, 0			; Read Call # (Linux)
-;	mov		rax, 0x2000003	; Read Call # (BSD)
+	mov		rax, READ_CALL	; Read Call #
 	syscall					; Call the system
-	test	rax, rax		; Check for error (Linux)
-	js		.error
-;	jc		.error			; Check for error (BSD)
+	JERR	.error
 	ret
 .error:
 	mov		rdx, rax		; Get error code
--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,3 +1,16 @@
+%ifidn __OUTPUT_FORMAT__, elf64
+	%assign WRITE_CALL 1
+	%macro JERR 1
+		test	rax, rax	; Check for error (Linux)
+		js		%1
+	%endmacro
+%elifidn __OUTPUT_FORMAT__, macho64
+	%assign WRITE_CALL 0x2000004
+	%macro JERR 1
+		jc		%1			; Check for error (BSD)
+	%endmacro
+%endif
+
 section	.text
 global	ft_write
 extern	__errno_location
@@ -5,12 +18,9 @@ extern	__errno_location
 ft_write:					; RDI, RSI, RDX - RAX
 	test	rdx, rdx		; Check for negative length
 	js		.error			;
-	mov		rax, 1			; Write Call # (Linux)
-;	mov		rax, 0x2000004	; Write Call # (BSD)
+	mov		rax, WRITE_CALL	; Write Call #
 	syscall					; Call the system
-	test	rax, rax		; Check for error (Linux)
-	js		.error
-;	jc		.error			; Check for error (BSD)
+	JERR	.error
 	ret
 .error:
 	mov		rdx, rax		; Get error code
