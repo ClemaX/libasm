0a5f245f804e887c0be7f0922be8aa19a230f2d6
Clément Hamada <clementhamada@protonmail.com>
2020-05-21 18:47:26 +0200

    Add test for ft_list_size

--- tests/srcs/units/test_ft_list_size.c
+++ tests/srcs/units/test_ft_list_size.c
@@ -15,4 +15,20 @@ int	unit_ft_list_size_rand(void)
 	return (!diff_i(i, expected));
 }
 
-int (*tests_ft_list_size[])(void) = {&unit_ft_list_size_rand, NULL};
+int	unit_ft_list_size_empty(void)
+{
+	t_list	*list;
+	int		expected;
+	int		i;
+
+	list = NULL;
+	expected = 0;
+	i = ft_list_size(list);
+	return (!diff_i(i, expected));
+}
+
+int (*tests_ft_list_size[])(void) = {
+	&unit_ft_list_size_empty,
+	&unit_ft_list_size_rand,
+	NULL
+};
