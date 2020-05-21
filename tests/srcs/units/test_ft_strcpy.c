4a7ffd2f0c1b65bf96dc07ccc9894ccacc8fccd9
Clément Hamada <clementhamada@protonmail.com>
2020-05-21 17:23:16 +0200

    Add tests for ft_strcpy

--- tests/srcs/units/test_ft_strcpy.c
+++ tests/srcs/units/test_ft_strcpy.c
@@ -1,17 +1,44 @@
 /* ************************************************************************** */
-/*                                                          LE - /            */
-/*                                                              /             */
-/*   test_ft_strcpy.c                                 .::    .:/ .      .::   */
-/*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
-/*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2020/02/09 17:42:21 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/09 18:01:31 by chamada     ###    #+. /#+    ###.fr     */
-/*                                                         /                  */
-/*                                                        /                   */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_strcpy.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chamada <chamada@student.le-101.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/09 17:42:21 by chamada           #+#    #+#             */
+/*   Updated: 2020/05/21 17:22:59 by chamada          ###   ########lyon.fr   */
+/*                                                                            */
 /* ************************************************************************** */
 
+
 #include <tests.h>
+
+int	unit_ft_strcpy_rand(void)
+{
+	static const int	len = 256;
+	char				*src = rand_key(len);
+	char				*dst;
+	int					diff;
+
+	dst = malloc(sizeof(*dst) * (len + 1));
+	ft_strcpy(dst, src);
+	diff = diff_s(dst, src);
+	free(dst);
+	return (!diff);
+}
+
+int	unit_ft_strcpy_empty(void)
+{
+	static const char	*src = "";
+	char				*dst;
+	int					diff;
+
+	dst = malloc(sizeof(*dst) * 1);
+	ft_strcpy(dst, src);
+	diff = diff_s(dst, src);
+	free(dst);
+	return (!diff);
+}
 
 int	unit_ft_strcpy_basic(void)
 {
@@ -26,4 +53,9 @@ int	unit_ft_strcpy_basic(void)
 	return (!diff);
 }
 
-int (*tests_ft_strcpy[])(void) = {&unit_ft_strcpy_basic, NULL};
+int (*tests_ft_strcpy[])(void) = {
+	&unit_ft_strcpy_basic,
+	&unit_ft_strcpy_empty,
+	&unit_ft_strcpy_rand,
+	NULL
+};
